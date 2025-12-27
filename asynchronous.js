defc07949cf2239dc894b8bbf260df797c60e2b9
Vasanth117 <vasanth111907@example.com>
2025-12-27 11:17:49 +0530

    addin the code for more reading and writing the files

--- asynchronous.js
+++ asynchronous.js
@@ -1,6 +1,17 @@
 const fs = require('fs');
 
-fs.readFile('./txt/start.txt','utf-8',(err,data)=>{
-console.log(data);
+fs.readFile('./txt/input.txt', 'utf-8', (err, data1) => {
+  if (err) return console.log(err);
+
+  fs.readFile('./txt/append.txt', 'utf-8', (err, data2) => {
+    if (err) return console.log(err);
+
+    fs.writeFile('./txt/final.txt', `${data1}\n${data2}`, 'utf-8', err => {
+      if (err) return console.log(err);
+      console.log('Your file has been written');
+    });
+  });
 });
+
 console.log('Will read file!');
+
